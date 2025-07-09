ac6f6e8243c51226ef69ef71fc979dad43d4cb17
muharrem <muharrem.demirel2324@gmail.com>
2025-07-09 15:36:32 +0300

    Add pytest configuration, update dependencies, and enhance error messages
    - Added pytest configuration in settings.json.
    - Included pyarrow as a dependency in pyproject.toml and uv.lock.
    - Improved test cases for DataLoader to validate file extensions.
    - Updated error messages for unsupported file extensions.
    - Added sample test data files for testing.

--- tests/data_loader/test_data_loader.py
+++ tests/data_loader/test_data_loader.py
@@ -1,15 +1,19 @@
 import pytest
 from uv_pck.src.data_loader import DataLoader
+import re 
 
 @pytest.fixture
 def data_loader():
     return DataLoader()
 
-     
-def test_check_if_file_extension_supported(data_loader):
-    # Test with a supported file extension
-    assert data_loader._check_if_file_extension_supported("data.csv") == ".csv"
+@pytest.mark.parametrize("extension", ['.csv', '.parquet'])
+def test_check_if_file_extension_returns_correct_result(data_loader, extension):
+    file_path = f'test{extension}'
 
-    with pytest.raises(ValueError):
+    assert data_loader._check_if_file_extension_supported(file_path) == extension
+
+def test_check_if_file_extension_throws_value_error_when_unsupported(data_loader):
+
+    with pytest.raises(ValueError, match= re.escape("File extension not supported: .txt Please use one of the following: ['.csv', '.parquet']")):
         data_loader._check_if_file_extension_supported("test.txt")
    
